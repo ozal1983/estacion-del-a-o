e5cbc7665f06022d513c5893257e6b669f017dbf
Oscar Alejandro Barraza Vargas <oscar_alejandro83@hotmail.com>
2021-10-08 21:20:03 -0500

    terminando ejercicio de estacion del año

---
+++ estaciones.js
@@ -0,0 +1,16 @@
+let mes=11;
+let estacion;
+
+if (mes==12||mes==1||mes==2){
+    estacion='Invierno'
+}
+else if(mes==3||mes==4||mes==5){
+    estacion='Primavera'
+}
+else if(mes==6||mes==7||mes==8){
+    estacion='Verano'
+}
+else{
+    estacion='Otoño'
+}
+console.log(estacion);
